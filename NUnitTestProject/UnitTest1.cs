dfc3fe6278dbe0a5318bd10f7d9861321de98c35
mohseen <mohseen.it@gmail.com>
2014-01-08 23:41:08 +0530

    seleniumTest

--- NUnitTestProject/UnitTest1.cs
+++ NUnitTestProject/UnitTest1.cs
@@ -4,16 +4,32 @@ using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
-
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
 namespace NUnitTestProject
 {
     [TestFixture]
     public class UnitTest1
     {
+        private IWebDriver _driver;
+        [SetUp]
+        public void Setup()
+        {
+            _driver = new FirefoxDriver();
+
+        }
+        [TearDown]
+        public void Tear()
+        {
+            _driver.Close();
+        }
         [Test]
         public void TestMethod1()
         {
-            Assert.AreEqual(2, 1);
+            _driver.Navigate().GoToUrl("http://google.com");
+            _driver.FindElement(By.Id("gbqfq")).SendKeys("Selenium");
+            _driver.FindElement(By.Id("gbqfba")).Click();
+            
         }
     }
 }
